71e2449959f54af8c7287253d8a307fc2af9c095
William <williamstam@gmail.com>
2021-11-23 12:55:56 +0000

    [Destination] Add placeholder for destination

--- src/blocks/Destination/Destination.js
+++ src/blocks/Destination/Destination.js
@@ -6,7 +6,32 @@ const Destination = React.forwardRef(function Destination(props, ref) {
   const { className, children, ...other } = props
   return (
     <div className={classes.root} ref={ref} {...other}>
-      <h2>DESTINATION</h2>
+      {/* planets, Title, description, distance, time */}
+
+      <img src="" alt="planet" />
+
+      <ul className={classes.planetList}>
+        <li className={classes.planet}>planet 1</li>
+        <li className={classes.planet}>planet 2</li>
+        <li className={classes.planet}>planet 3</li>
+        <li className={classes.planet}>planet 4</li>
+      </ul>
+
+      <h1 className={classes.title}>Planet</h1>
+      <p className={classes.description}>Description</p>
+
+      <hr />
+      <div>
+        <div className={classes.distanceContainer}>
+          <p className={classes.distance}>Distance</p>
+          <p className={classes.distanceKm}>384,400KM</p>
+        </div>
+
+        <div className={classes.timeContainer}>
+          <p className={classes.time}>Time</p>
+          <p className={classes.days}>3 days</p>
+        </div>
+      </div>
     </div>
   )
 })
