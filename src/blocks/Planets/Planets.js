c8ccef01f78adb308c553aa536443a5701fe138f
William <williamstam@gmail.com>
2021-11-25 13:25:23 +0000

    [Planets] Add state

--- src/blocks/Planets/Planets.js
+++ src/blocks/Planets/Planets.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'next/link'
 import classes from './Planets.module.css'
 
 const Planets = React.forwardRef(function Planets(props, ref) {
@@ -8,6 +7,8 @@ const Planets = React.forwardRef(function Planets(props, ref) {
   const [currentImg, setCurrentImg] = React.useState(api[0].images.img)
   const [currentMoon, setCurrentMoon] = React.useState('moon')
   const [currentDescription, setCurrentDescription] = React.useState(api[0].description)
+  const [currentDistance, setCurrentDistance] = React.useState(api[0].distance)
+  const [currentTravel, setCurrentTravel] = React.useState(api[0].travel)
 
   return (
     <div className={classes.root} ref={ref} {...other}>
@@ -22,7 +23,13 @@ const Planets = React.forwardRef(function Planets(props, ref) {
                 onClick={() =>
                   setCurrentMoon(
                     moon.name,
-                    setCurrentDescription(moon.description, setCurrentImg(moon.images.img)),
+                    setCurrentDescription(
+                      moon.description,
+                      setCurrentImg(
+                        moon.images.img,
+                        setCurrentDistance(moon.distance, setCurrentTravel(moon.travel)),
+                      ),
+                    ),
                   )
                 }
               >
@@ -31,8 +38,18 @@ const Planets = React.forwardRef(function Planets(props, ref) {
             </div>
           ))}
         </div>
-        <h3>{currentMoon}</h3>
-        <p>{currentDescription}</p>
+        <div className={classes.textContainer}>
+          <h3>{currentMoon}</h3>
+          <p className={classes.description}>{currentDescription}</p>
+          <div className={classes.line} />
+        </div>
+
+        <div className={classes.test}>
+          <h5 className={classes.distanceHeader}>AVG. DISTANCE</h5>
+          <p className={classes.distance}>{currentDistance}</p>
+          <h5 className={classes.distanceHeader}>EST. TRAVEL TIME</h5>
+          <p className={classes.distance}>{currentTravel}</p>
+        </div>
       </div>
     </div>
   )
