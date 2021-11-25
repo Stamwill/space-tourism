cf98560d3e37f29d776e65c172107b14fe096411
William <williamstam@gmail.com>
2021-11-25 20:19:03 +0000

    [crew.js] import Crewmember block and pass api

--- src/pages/crew.js
+++ src/pages/crew.js
@@ -2,17 +2,16 @@ import * as React from 'react'
 import { crew as api, crewBg } from '../api/mock'
 import Picture from '../components/Picture'
 import PageTracker from '../blocks/PageTracker'
+import CrewMember from '../blocks/CrewMember/CrewMember'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  console.log('api: ', api)
-  console.log('crewbg: ', crewBg)
-
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <Picture api={crewBg} />
         <PageTracker />
+        <CrewMember api={api} />
       </main>
     </div>
   )
