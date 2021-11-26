ad1a7e4dc96019c3b560b41abc20b431435d4bf7
William <williamstam@gmail.com>
2021-11-26 17:41:02 +0000

    [crew] Remove embla

--- src/pages/crew.js
+++ src/pages/crew.js
@@ -3,7 +3,6 @@ import { crew as api, crewBg } from '../api/mock'
 import Picture from '../components/Picture'
 import PageTracker from '../blocks/PageTracker'
 import CrewMember from '../blocks/CrewMember/CrewMember'
-import EmblaCarousel from '../components/EmblaCarousel'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
