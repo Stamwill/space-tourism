345ab42d3b5ea2b09b2a4343c7a32f8d57223ef7
William <williamstam@gmail.com>
2021-12-20 11:37:02 +0000

    [PageTracker] Add props

--- src/blocks/PageTracker/PageTracker.js
+++ src/blocks/PageTracker/PageTracker.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 import classes from './PageTracker.module.css'
 
 const PageTracker = React.forwardRef(function PageTracker(props, ref) {
-  const { className, children, pageNumber, ...other } = props
+  const { className, children, pageNumber, pageText, ...other } = props
   return (
     <div className={classes.root} ref={ref} {...other}>
       <span className={classes.num}>{pageNumber}</span>
-      <h5 className={classes.text}>Test</h5>
+      <h5 className={classes.text}>{pageText}</h5>
     </div>
   )
 })
@@ -16,6 +16,7 @@ PageTracker.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
   pageNumber: PropTypes.string,
+  pageText: PropTypes.string,
 }
 
 export default PageTracker
