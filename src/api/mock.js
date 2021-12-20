4ada00979d000b8d8cc053857d5217bca58ee543
William <williamstam@gmail.com>
2021-12-20 11:32:05 +0000

    [api/mock] Add data for technology

--- src/api/mock.js
+++ src/api/mock.js
@@ -103,12 +103,23 @@ export const crew = [
     bio: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.',
   },
 ]
+
+export const technologyBg = [
+  {
+    background: {
+      desktop: '/images/technology/background-technology-desktop.jpg',
+      tablet: '/images/technology/background-technology-tablet.jpg',
+      mobile: '/images/technology/background-technology-mobile.jpg',
+    },
+  },
+]
+
 export const technology = [
   {
     name: 'Launch vehicle',
     images: {
-      portrait: './assets/technology/image-launch-vehicle-portrait.jpg',
-      landscape: './assets/technology/image-launch-vehicle-landscape.jpg',
+      img: '/images/technology/image-launch-vehicle-portrait.jpg',
+      landscape: '/images/technology/image-launch-vehicle-landscape.jpg',
     },
     description:
       "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
@@ -116,8 +127,8 @@ export const technology = [
   {
     name: 'Spaceport',
     images: {
-      portrait: './assets/technology/image-spaceport-portrait.jpg',
-      landscape: './assets/technology/image-spaceport-landscape.jpg',
+      img: '/images/technology/image-spaceport-portrait.jpg',
+      landscape: '/images/technology/image-spaceport-landscape.jpg',
     },
     description:
       'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
@@ -125,8 +136,8 @@ export const technology = [
   {
     name: 'Space capsule',
     images: {
-      portrait: './assets/technology/image-space-capsule-portrait.jpg',
-      landscape: './assets/technology/image-space-capsule-landscape.jpg',
+      img: '/images/technology/image-space-capsule-portrait.jpg',
+      landscape: '/images/technology/image-space-capsule-landscape.jpg',
     },
     description:
       "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
@@ -169,6 +180,6 @@ export const nav = [
   {
     num: '03',
     label: 'technology',
-    href: '/',
+    href: '/technology',
   },
 ]
