e576781df3b380161f73e485053cbe180d508f47
William <williamstam@gmail.com>
2021-12-20 12:02:33 +0000

    [mock] add data to technology

--- src/api/mock.js
+++ src/api/mock.js
@@ -123,6 +123,7 @@ export const technology = [
     },
     description:
       "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+    num: 1,
   },
   {
     name: 'Spaceport',
@@ -132,6 +133,7 @@ export const technology = [
     },
     description:
       'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
+    num: 2,
   },
   {
     name: 'Space capsule',
@@ -141,6 +143,7 @@ export const technology = [
     },
     description:
       "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+    num: 3,
   },
 ]
 export const home = [
