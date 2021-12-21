3a8f2bed50e1eda26945afe1256d42e28e9a997a
William <williamstam@gmail.com>
2021-12-21 16:55:03 +0000

    [EmblaButton] add children

--- src/components/EmblaCarousel/partials/EmblaButton.js
+++ src/components/EmblaCarousel/partials/EmblaButton.js
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types'
 import clsx from 'clsx'
 import classes from './EmblaButton.module.css'
 
-const EmblaButton = ({ selected, onClick }) => (
+const EmblaButton = ({ selected, onClick, children }) => (
   <button
     className={clsx(classes.button, { [classes.isSelected]: selected })}
     type="button"
     onClick={onClick}
-  />
+  >
+    {children}
+  </button>
 )
 
 EmblaButton.propTypes = {
   selected: PropTypes.bool,
   onClick: PropTypes.func,
+  children: PropTypes.node,
 }
 
 export default EmblaButton
