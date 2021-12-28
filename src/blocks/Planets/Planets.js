ee18eb99be479a98ebed4b8cdc6b2073b2ce97d5
William <williamstam@gmail.com>
2021-12-28 13:09:20 +0000

    [Planets] Rearrange placement, add div

--- src/blocks/Planets/Planets.js
+++ src/blocks/Planets/Planets.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import PageTracker from '../PageTracker/PageTracker'
+import Section from '../../components/Section'
 import classes from './Planets.module.css'
 
 const Planets = React.forwardRef(function Planets(props, ref) {
@@ -12,55 +13,58 @@ const Planets = React.forwardRef(function Planets(props, ref) {
   const [currentTravel, setCurrentTravel] = React.useState(api[0].travel)
 
   return (
-    <div className={classes.root} ref={ref} {...other}>
+    <Section className={classes.root} ref={ref} {...other}>
       <PageTracker pageNum={`01`} pageLabel={`PICK YOUR DESTINATION`} />
-      <img className={classes.img} src={currentImg} alt="test" />
       <div className={classes.container}>
-        <div className={classes.buttons}>
-          {api.map((moon, idx) => (
-            <div key={idx}>
-              <button
-                className={classes.button}
-                type="button"
-                onClick={() =>
-                  setCurrentMoon(
-                    moon.name,
-                    setCurrentDescription(
-                      moon.description,
-                      setCurrentImg(
-                        moon.images.img,
-                        setCurrentDistance(moon.distance, setCurrentTravel(moon.travel)),
+        <img className={classes.img} src={currentImg} alt="test" />
+
+        <div className={classes.subContainer}>
+          <div className={classes.buttons}>
+            {api.map((moon, idx) => (
+              <div key={idx}>
+                <button
+                  className={classes.button}
+                  type="button"
+                  onClick={() =>
+                    setCurrentMoon(
+                      moon.name,
+                      setCurrentDescription(
+                        moon.description,
+                        setCurrentImg(
+                          moon.images.img,
+                          setCurrentDistance(moon.distance, setCurrentTravel(moon.travel)),
+                        ),
                       ),
-                    ),
-                  )
-                }
-              >
-                {moon.name}
-              </button>
+                    )
+                  }
+                >
+                  {moon.name}
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className={classes.textContainer}>
+            <h3>{currentMoon}</h3>
+            <p className={classes.description}>{currentDescription}</p>
+            <div className={classes.line} />
+          </div>
+
+          {/* Create block for moon and description? */}
+
+          <div className={classes.test}>
+            <div>
+              <h5 className={classes.distanceHeader}>AVG. DISTANCE</h5>
+              <p className={classes.distance}>{currentDistance}</p>
             </div>
-          ))}
-        </div>
-        <div className={classes.textContainer}>
-          <h3>{currentMoon}</h3>
-          <p className={classes.description}>{currentDescription}</p>
-          <div className={classes.line} />
-        </div>
-
-        {/* Create block for moon and description? */}
-
-        <div className={classes.test}>
-          <div>
-            <h5 className={classes.distanceHeader}>AVG. DISTANCE</h5>
-            <p className={classes.distance}>{currentDistance}</p>
+            <div>
+              <h5 className={classes.distanceHeader}>EST. TRAVEL TIME</h5>
+              <p className={classes.distance}>{currentTravel}</p>
+            </div>
           </div>
-          <div>
-            <h5 className={classes.distanceHeader}>EST. TRAVEL TIME</h5>
-            <p className={classes.distance}>{currentTravel}</p>
-          </div>
+          {/* Create block for distance ? */}
         </div>
-        {/* Create block for distance ? */}
       </div>
-    </div>
+    </Section>
   )
 })
 
