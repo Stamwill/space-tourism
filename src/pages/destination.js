849ce948731a31b13a5f50cc6dd915a51e2b8b04
William <williamstam@gmail.com>
2022-01-14 15:58:42 +0000

    [destination] Add Pagetracker

--- src/pages/destination.js
+++ src/pages/destination.js
@@ -1,14 +1,17 @@
 import * as React from 'react'
 import { destinations as api, destinationBg } from '../api/mock'
 import Picture from '../components/Picture'
+import PageTracker from '../blocks/PageTracker/PageTracker'
 import Planets from '../blocks/Planets'
 import styles from '../styles/Home.module.css'
+import classes from '../styles/destination.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
+      <main className={classes.main}>
         <Picture api={destinationBg} />
+        <PageTracker pageNum={`01`} pageLabel={`PICK YOUR DESTINATION`} />
         <Planets api={api} />
       </main>
     </div>
